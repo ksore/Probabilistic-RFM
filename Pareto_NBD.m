749cc9eb1e56432f985376a7c99d58464842b76d
Raphael Varieras <raphael.varieras@radiangen.com>
2017-03-24 14:32:47 -0700

    Added conditional expectations and CLV

--- Pareto_NBD.m
+++ Pareto_NBD.m
@@ -69,7 +69,7 @@ for i = 1:cohort_n
     tx = table2array(patients(patients.cohort == i,'last_repeat'));
     T = table2array(patients(patients.cohort == i,'observation_time'));
     p2x = table2array(patients(patients.cohort == i,'holdout_visits'));
-    zbar = table2array(patients(patients.cohort == i,'mean_value'));
+    zbar = table2array(patients(patients.cohort == i,'mean_rev'));
     
     % Calculate Pareto-NBD Frequency and Churn Model Coefficients
     
@@ -98,6 +98,12 @@ x = 1:1000;
 clf(spendmodel); 
 
 for i = 1:cohort_n
+    p1x = table2array(patients(patients.cohort == i,'study_visits'));
+    tx = table2array(patients(patients.cohort == i,'last_repeat'));
+    T = table2array(patients(patients.cohort == i,'observation_time'));
+    p2x = table2array(patients(patients.cohort == i,'holdout_visits'));
+    zbar = table2array(patients(patients.cohort == i,'mean_rev'));
+        
     subplot(floor(sqrt(cohort_n)),floor(sqrt(cohort_n))+1,i)
     histogram(zbar,'Normalization','pdf', 'BinLimits',[0,1000],'BinWidth',10);
     hold on;
@@ -117,165 +123,246 @@ clear i x;
 days_in_cohort = weeks_in_cohort * 7;
 days_in_study = weeks_in_study * 7;
 
-% determine cohort size by day of trial
-ns = [];
-for i = 1:days_in_cohort
-    ns(i) = sum( round(T,2) == round((days_in_study+1-i)/7,2));
+x_axis_max = 80;
+y_axis_max_rpt=700;
+y_axis_max_cum=20000;
+
+for i = 1:cohort_n
+    p1x = table2array(patients(patients.cohort == i,'study_visits'));
+    tx = table2array(patients(patients.cohort == i,'last_repeat'));
+    T = table2array(patients(patients.cohort == i,'observation_time'));
+    p2x = table2array(patients(patients.cohort == i,'holdout_visits'));
+    zbar = table2array(patients(patients.cohort == i,'mean_rev'));
+    
+    % determine cohort size by day of trial
+    ns = [];
+    for j = 1:days_in_cohort
+        ns(j) = sum( round(T,2) == round((days_in_study+1-j)/7,2));
+    end
+
+    % generate sales cumulative forecast
+    endwk = weeks_in_study * 2 ;
+    endday = endwk*7; 
+
+    tmp1 = cohort_results.r(i)*cohort_results.beta(i)/ ...
+        (cohort_results.alpha(i)*(cohort_results.s(i)-1));
+    tmpcumsls1 = [];
+    for j = 1:endday
+        tmp2 = (cohort_results.beta(i)/(cohort_results.beta(i)+j/7))^(cohort_results.s(i)-1);
+        tmpcumsls1(j) = tmp1*(1-tmp2);
+    end
+
+    tmpcumsls2 = zeros(days_in_cohort,endday);
+    for j = 1:days_in_cohort
+        tmpcumsls2(j,:) = [ zeros(1,j) tmpcumsls1(1:endday-j) ];
+    end
+
+    cumrptsls = [];
+    dailysls = ns*tmpcumsls2;
+    for j = 1:endwk
+        cumrptsls(j) = dailysls(j*7);
+    end     
+
+    chart_offset = (i-1)*weeks_in_cohort;
+
+    % create tracking plot of cumulative repeat sales (pred. vs actual)
+
+    figure(cumrptslsfig);
+    subplot(floor(sqrt(cohort_n)),floor(sqrt(cohort_n))+1,i)
+
+    plot(1:endwk,weekly_cumul_visits(1:endwk,i),'k', ...
+        1+chart_offset:endwk+chart_offset,cumrptsls,'b--');
+    axis([0 x_axis_max 0 y_axis_max_cum]);
+    xlabel('Week'); ylabel('Cumulative Repeat Transactions');
+    legend('Actual','Model','Location','SouthEast');
+    % print -depsc 'cumrptsls.eps'
+
+    % create tracking plot of weekly repeat sales (pred. vs actual)
+    figure(rptslsfig);
+    subplot(floor(sqrt(cohort_n)),floor(sqrt(cohort_n))+1,i)
+
+    incrptsls = [ cumrptsls(1) diff(cumrptsls) ];
+    plot(1:endwk,weekly_visits(1:endwk,i),'k', ...
+        1+chart_offset:endwk+chart_offset,incrptsls,'b--');
+    axis([0 x_axis_max 0 y_axis_max_rpt]);
+    xlabel('Week'); ylabel('Weekly Repeat Transactions');
+    legend('Actual','Model','Location','SouthEast');
+    % print -depsc 'incrptsls.eps'
+
 end
 
-% generate sales cumulative forecast
-endwk = weeks_in_study * 2 ;
-endday = endwk*7; 
+clear ns tmp1 tmp2 tmpcumsls1 tmpcumsls2 chart_offset i j x_axis_max ...
+    y_axis_max_rpt y_axis_max_cum cumrptsls incrptsls dailysls;
 
-tmp1 = r*beta/(alpha*(s-1));
-tmpcumsls1 = [];
-for i = 1:endday
-    tmp2 = (beta/(beta+i/7))^(s-1);
-    tmpcumsls1(i) = tmp1*(1-tmp2);
-end
-
-tmpcumsls2 = zeros(days_in_cohort,endday);
-for i = 1:days_in_cohort
-    tmpcumsls2(i,:) = [ zeros(1,i) tmpcumsls1(1:endday-i) ];
-end
-
-cumrptsls = [];
-dailysls = ns*tmpcumsls2;
-for i = 1:endwk
-    cumrptsls(i) = dailysls(i*7);
-end     
-
-chart_offset = (cohort-1)*weeks_in_cohort;
-
-% create tracking plot of cumulative repeat sales (pred. vs actual)
-
-figure(cumrptslsfig);
-plot(1:endwk,weekly_cumul_visits(1:endwk,cohort),'k', ...
-    1+chart_offset:endwk+chart_offset,cumrptsls,'b--');
-xlabel('Week'); ylabel('Cumulative Repeat Transactions');
-legend('Actual','Model','Location','SouthEast');
-% print -depsc 'cumrptsls.eps'
-
-% create tracking plot of weekly repeat sales (pred. vs actual)
-figure(rptslsfig);
-incrptsls = [ cumrptsls(1) diff(cumrptsls) ];
-plot(1:endwk,weekly_visits(1:endwk,cohort),'k', ...
-    1+chart_offset:endwk+chart_offset,incrptsls,'b--');
-xlabel('Week'); ylabel('Weekly Repeat Transactions');
-legend('Actual','Model','Location','SouthEast');
-% print -depsc 'incrptsls.eps'
-
-clear ns tmp1 tmp2 tmpcumsls1 tmpcumsls2
+% %% 
+% % Script to compute P(active|p1x,tx,T) for Pareto/NBD model.
+% % Also creates a plot comparing average P(active|p1x,tx,T) with the
+% % observed proportion of customers active in the second period by p1x.
+% %
+% 
+% % compute P(active|p1x,tx,T)
+% 
+% maxab = max(alpha,beta);
+% absab = abs(alpha-beta);
+% param2 = s+1;
+% if alpha < beta
+%     param2 = r+p1x;
+% end    
+% 
+% F0 = (alpha+T).^(r+p1x).*(beta+T).^s;
+% F1=h2f1(r+s+p1x,param2,r+s+p1x+1,absab./(maxab+tx))./...
+%     ((maxab+tx).^(r+s+p1x));
+% F2=h2f1(r+s+p1x,param2,r+s+p1x+1,absab./(maxab+T))./...
+%     ((maxab+T).^(r+s+p1x));
+% pactive = 1./(1+(s./(r+s+p1x)).*F0 .*(F1-F2));
+% 
+% % compute average P(active|p1x,tx,T) and determine the proportion of
+% % customers buying in the second 39 weeks for each level of p1x
+% pa_actual = zeros(max(p1x)+1,1);
+% pa_est = zeros(max(p1x)+1,1);
+% np1x = zeros(max(p1x)+1,1);
+% for y = unique(p1x)'
+%     isx = find(p1x==y);
+%     np1x(y+1) = length(isx);
+%     pa_actual(y+1) = sum(p2x(isx)>0)/np1x(y+1);
+%     pa_est(y+1) = sum(pactive(isx))/np1x(y+1);
+% end
+% 
+% % create right-censored version for plot
+% censor = 12;  % right-censor at 12+
+% denom = sum(np1x(censor+1:length(np1x)));
+% 
+% pa_act_cen = pa_actual(1:censor);
+% pa_act_cen(censor+1) = (np1x(censor+1:length(np1x))'*...
+%     pa_actual(censor+1:length(np1x)))/denom;
+% 
+% pa_est_cen = pa_est(1:censor);
+% pa_est_cen(censor+1) = (np1x(censor+1:length(np1x))'*...
+%     pa_est(censor+1:length(np1x)))/denom;
+% 
+% subplot(2,2,3)
+% 
+% plot(0:censor,pa_act_cen,'k',0:censor,pa_est_cen,'kp--');
+% legend('Empirical','Pareto/NBD','Location','SouthEast');
+% xlabel(strcat('Transactions in Study Period (',string(weeks_in_study),' weeks)')); 
+% ylabel('Probability of still being a customer');
+% axis([-.3 12.3 0 1]);
+% xticks([0 1 2 3 4 5 6 7 8 9 10 11 12])
+% xticklabels({ ' 0', ' 1', ' 2', ' 3', ' 4', ' 5', ' 6', '7', '8', '9', '10', '11', '12+' });
+% grid on
+% grid minor
+% % print -depsc 'pactive_grouped.eps'
+% 
+% clear denom y isx np1x F0 F1 F2 absab
 
 %% 
-% Script to compute P(active|p1x,tx,T) for Pareto/NBD model.
-% Also creates a plot comparing average P(active|p1x,tx,T) with the
-% observed proportion of customers active in the second period by p1x.
+% Script to compute conditional expectations
 %
-
-% compute P(active|p1x,tx,T)
-
-maxab = max(alpha,beta);
-absab = abs(alpha-beta);
-param2 = s+1;
-if alpha < beta
-    param2 = r+p1x;
-end    
-
-F0 = (alpha+T).^(r+p1x).*(beta+T).^s;
-F1=h2f1(r+s+p1x,param2,r+s+p1x+1,absab./(maxab+tx))./...
-    ((maxab+tx).^(r+s+p1x));
-F2=h2f1(r+s+p1x,param2,r+s+p1x+1,absab./(maxab+T))./...
-    ((maxab+T).^(r+s+p1x));
-pactive = 1./(1+(s./(r+s+p1x)).*F0 .*(F1-F2));
-
-% compute average P(active|p1x,tx,T) and determine the proportion of
-% customers buying in the second 39 weeks for each level of p1x
-pa_actual = zeros(max(p1x)+1,1);
-pa_est = zeros(max(p1x)+1,1);
-np1x = zeros(max(p1x)+1,1);
-for y = unique(p1x)'
-    isx = find(p1x==y);
-    np1x(y+1) = length(isx);
-    pa_actual(y+1) = sum(p2x(isx)>0)/np1x(y+1);
-    pa_est(y+1) = sum(pactive(isx))/np1x(y+1);
-end
-
-% create right-censored version for plot
-censor = 12;  % right-censor at 12+
-denom = sum(np1x(censor+1:length(np1x)));
-
-pa_act_cen = pa_actual(1:censor);
-pa_act_cen(censor+1) = (np1x(censor+1:length(np1x))'*...
-    pa_actual(censor+1:length(np1x)))/denom;
-
-pa_est_cen = pa_est(1:censor);
-pa_est_cen(censor+1) = (np1x(censor+1:length(np1x))'*...
-    pa_est(censor+1:length(np1x)))/denom;
-
-subplot(2,2,3)
-
-plot(0:censor,pa_act_cen,'k',0:censor,pa_est_cen,'kp--');
-legend('Empirical','Pareto/NBD','Location','SouthEast');
-xlabel(strcat('Transactions in Study Period (',string(weeks_in_study),' weeks)')); 
-ylabel('Probability of still being a customer');
-axis([-.3 12.3 0 1]);
-xticks([0 1 2 3 4 5 6 7 8 9 10 11 12])
-xticklabels({ ' 0', ' 1', ' 2', ' 3', ' 4', ' 5', ' 6', '7', '8', '9', '10', '11', '12+' });
-grid on
-grid minor
-% print -depsc 'pactive_grouped.eps'
-
-clear denom y isx np1x F0 F1 F2 absab
-
-%% 
-
-% Script to compute the Pareto/NBD conditional expectations
-%
-% Assumes -- the parameter estimates are contained in the vector params
-%         -- the individual-level customer data are residing in memory
-%         -- pactive (for each customer) resides in memory
 
 t = weeks_in_study;  % period for which conditional expectations are to be computed
 
-tmp1 = (r+p1x).*(beta+T)./((alpha+T).*(s-1));
-tmp2 = ((beta+T)./(beta+T + t)).^(s-1);
-ce = tmp1.*(1-tmp2).*pactive;
+for i = 1:size(patients)
+    cohort = patients.cohort(i);
+    study_visits = patients.study_visits(i);
+    obs_time = patients.observation_time(i);
+    lst_rpt = patients.last_repeat(i);
+    avg_purchase = patients.mean_rev(i);
+    
+    if cohort <=cohort_n
+        
+        r = cohort_results.r(cohort);
+        alpha= cohort_results.alpha(cohort);
+        s= cohort_results.s(cohort);
+        beta= cohort_results.beta(cohort);
+        p= cohort_results.p(cohort);
+        q= cohort_results.q(cohort);
+        gam= cohort_results.gamma(cohort);
 
-% compute average E[Y(t)|p1x,tx,T] and average actual number of 
-% transactions in the second 39 weeks for each level of p1x
-ce_act = zeros(max(p1x)+1,1);
-ce_est = zeros(max(p1x)+1,1);
-np1x = zeros(max(p1x)+1,1);
-for y = unique(p1x)'
-    isx = find(p1x==y);
-    np1x(y+1) = length(isx);
-    ce_act(y+1) = sum(p2x(isx))/np1x(y+1);
-    ce_est(y+1) = sum(ce(isx))/np1x(y+1);
+        % Pareto/NBD Expectations
+        
+        maxab = max(alpha,beta);
+        absab = abs(alpha-beta);
+        param2 = s+1;
+        if alpha < beta
+            param2 = r+study_visits;
+        end  
+
+        F0 = (alpha+obs_time)^(r+study_visits)*(beta+obs_time)^s;
+        F1 = h2f1(r+s+study_visits,param2,r+s+study_visits+1,absab/(maxab+lst_rpt))/...
+            ((maxab+lst_rpt)^(r+s+study_visits));
+        F2 = h2f1(r+s+study_visits,param2,r+s+study_visits+1,absab/(maxab+obs_time))/...
+            ((maxab+obs_time)^(r+s+study_visits));
+        A0 = F1-F2;
+        pactive = (1+(s/(r+s+study_visits))*F0*A0)^(-1);
+
+        tmp1 = (r+study_visits)*(beta+obs_time)/(alpha+obs_time)/(s-1);
+        tmp2 = ((beta+obs_time)/(beta+obs_time + t))^(s-1);
+        ce = tmp1*(1-tmp2)*pactive;
+
+        patients.exp_tr(i) = ce;
+        
+        % Gamma-gamma Expectations
+
+        patients.exp_sp(i) = p*(gam+(study_visits+1)*avg_purchase) / ...
+            (p*(study_visits+1)+q-1);
+        
+        % Discounted Expected Transactions
+        
+        delta = log(1+0.1)/52;
+        
+        L = gamma(r+study_visits)*alpha^r*beta^s/gamma(r) * ...
+            (1/F0 + (s/(r+s+study_visits)) * A0);
+        patients.DET(i) = alpha^r * beta^s * delta^(s-1) * gamma(r+study_visits+1) * ...
+            kummerU(s,s,delta*(beta+obs_time)) / ...
+            (gamma(r)*(alpha+obs_time)^(r+study_visits+1) * L);
+        
+        % CLV !!!
+        
+        patients.CLV(i) = patients.DET(i) * patients.exp_sp(i);
+        
+    end
+    
 end
 
-% create right-censored version for plot
-censor = 12;  % right-censor at 12+
-denom = sum(np1x(censor+1:length(np1x)));
+clear cohort study_visits  obs_time lst_rpt avg_purchase r alpha s beta ...
+    p q gam F0 F1 F2 A0 pactive delta
 
-ce_act_cen = ce_act(1:censor);
-ce_act_cen(censor+1) = (np1x(censor+1:length(np1x))'...
-    *ce_act(censor+1:length(np1x)))/denom;
 
-ce_est_cen = ce_est(1:censor);
-ce_est_cen(censor+1) = (np1x(censor+1:length(np1x))'...
-    *ce_est(censor+1:length(np1x)))/denom;
 
-subplot(2,2,4)
-
-plot([0:censor],ce_act_cen,'k',[0:censor],ce_est_cen,'kp--');
-legend('Actual','Pareto/NBD','Location','SouthEast');
-xlabel(strcat('Transactions in Study Period (',string(weeks_in_study),' weeks)')); 
-ylabel(strcat('Average Transactions in Holdout Period (',string(weeks_in_study),' weeks)'));
-xticks([0 1 2 3 4 5 6 7 8 9 10 11 12])
-xticklabels({ ' 0', ' 1', ' 2', ' 3', ' 4', ' 5', ' 6', '7', '8', '9', '10', '11', '12+' });
-grid on
-grid minor
-% print -depsc 'ce_plot.eps'
-
-clear tmp1 tmp2 y isx
+% % compute average E[Y(t)|p1x,tx,T] and average actual number of 
+% % transactions in the second 39 weeks for each level of p1x
+% ce_act = zeros(max(p1x)+1,1);
+% ce_est = zeros(max(p1x)+1,1);
+% np1x = zeros(max(p1x)+1,1);
+% for y = unique(p1x)'
+%     isx = find(p1x==y);
+%     np1x(y+1) = length(isx);
+%     ce_act(y+1) = sum(p2x(isx))/np1x(y+1);
+%     ce_est(y+1) = sum(ce(isx))/np1x(y+1);
+% end
+% 
+% % create right-censored version for plot
+% censor = 12;  % right-censor at 12+
+% denom = sum(np1x(censor+1:length(np1x)));
+% 
+% ce_act_cen = ce_act(1:censor);
+% ce_act_cen(censor+1) = (np1x(censor+1:length(np1x))'...
+%     *ce_act(censor+1:length(np1x)))/denom;
+% 
+% ce_est_cen = ce_est(1:censor);
+% ce_est_cen(censor+1) = (np1x(censor+1:length(np1x))'...
+%     *ce_est(censor+1:length(np1x)))/denom;
+% 
+% subplot(2,2,4)
+% 
+% plot([0:censor],ce_act_cen,'k',[0:censor],ce_est_cen,'kp--');
+% legend('Actual','Pareto/NBD','Location','SouthEast');
+% xlabel(strcat('Transactions in Study Period (',string(weeks_in_study),' weeks)')); 
+% ylabel(strcat('Average Transactions in Holdout Period (',string(weeks_in_study),' weeks)'));
+% xticks([0 1 2 3 4 5 6 7 8 9 10 11 12])
+% xticklabels({ ' 0', ' 1', ' 2', ' 3', ' 4', ' 5', ' 6', '7', '8', '9', '10', '11', '12+' });
+% grid on
+% grid minor
+% % print -depsc 'ce_plot.eps'
+% 
+% clear tmp1 tmp2 y isx
