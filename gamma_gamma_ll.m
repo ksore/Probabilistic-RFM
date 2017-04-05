6430da56e77bd7101d286875d48e71f7cca69a62
Raphael Varieras <raphael.varieras@radiangen.com>
2017-04-04 20:12:30 -0700

    Including the zeros

--- gamma_gamma_ll.m
+++ gamma_gamma_ll.m
@@ -8,16 +8,17 @@ p = param(1);
 q = param(2);
 gam = param(3);
 
-func = gammaln(p*p1x+q) - gammaln(p*p1x) - gammaln(q) + ...
-    q.*log(gam)+(p*p1x-1).*log(zbar) + (p*p1x) .* log(p1x) - ...
-    (p*p1x+q) .* log(gam+p1x.*zbar);
-    
+% func = gammaln(p*p1x+q) - gammaln(p*p1x) - gammaln(q) + ...
+%     q.*log(gam)+(p*p1x-1).*log(zbar) + (p*p1x) .* log(p1x) - ...
+%     (p*p1x+q) .* log(gam+p1x.*zbar);
+func = gammaln(p*(p1x+1)+q) - gammaln(p*(p1x+1)) - gammaln(q) + ...
+    q.*log(gam)+(p*(p1x+1)-1).*log(zbar) + (p*(p1x+1)) .* log((p1x+1)) - ...
+    (p*(p1x+1)+q) .* log(gam+(p1x+1).*zbar);
+
 accum = 0;
 
 for i = 1:length(func)
-    if p1x(i)>0
-        accum = accum + func(i);
-    end
+    accum = accum + func(i);
 end
 
 f = - accum;
